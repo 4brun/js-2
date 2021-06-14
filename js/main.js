9b0684fce92554754806a7d52adb80caf0f3b53d
Dmitriy <dabrun@gmail.com>
2021-06-14 13:37:27 +0300

    добавил метод для вычисления суммы товаров

--- js/main.js
+++ js/main.js
@@ -1,38 +1,48 @@
-class ProductList{
-    constructor(container='.products'){
+class ProductList {
+    constructor(container = '.products') {
         this.container = container;
         this.goods = [];
         this._fetchProducts();
-        this.render();//вывод товаров на страницу
+        this.render(); //вывод товаров на страницу
+        this.getSumm(); //вывод суммы товаров в alert
     }
-    _fetchProducts(){
+    _fetchProducts() {
         this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
         ];
     }
-    
-    render(){
+
+    getSumm() {
+        let total = 0;
+        this.goods.forEach(e => {
+            total += e.price;
+        });
+        alert(`Сумма товаров: ${total}`);
+    }
+
+    render() {
         const block = document.querySelector(this.container);
-        for(let product of this.goods){
+        for (let product of this.goods) {
             const item = new ProductItem(product);
-             block.insertAdjacentHTML("beforeend",item.render());
-//           block.innerHTML += item.render();
+            block.insertAdjacentHTML("beforeend", item.render());
+            //           block.innerHTML += item.render();
         }
     }
 }
 
-class ProductItem{
-    constructor(product,img='https://via.placeholder.com/200x150'){
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
         this.title = product.title;
         this.id = product.id;
         this.price = product.price;
         this.img = img;
+
     }
-    render(){
-           return `<div class="product-item">
+    render() {
+        return `<div class="product-item">
                 <img src="${this.img}">
                 <h3>${this.title}</h3>
                 <p>${this.price}</p>
